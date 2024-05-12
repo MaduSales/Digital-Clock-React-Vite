fab2bb68534c2a738331981ab4f89a75ae67a6cc
Maria Eduarda <maria.mirandadev@gmail.com>
2024-05-11 22:56:52 -0300

    relogio repository

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Relogio from './components/Relogio/Relogio'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Relogio/>
+    </div>
+  )
+}
+
